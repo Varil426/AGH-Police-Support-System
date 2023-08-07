3f17aed885074f1f6194b95eda142038179ea0f0
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-08-07 21:31:34 +0200

    IEvent - Added CreatedAt

--- PoliceSupportSystem/Shared.Application.Integration/Events/IEvent.cs
+++ PoliceSupportSystem/Shared.Application.Integration/Events/IEvent.cs
@@ -2,4 +2,5 @@
 
 public interface IEvent
 {
+    DateTimeOffset CreatedAt { get; }
 }
--- PoliceSupportSystem/Shared.Application.Integration/Events/TestEvent.cs
+++ PoliceSupportSystem/Shared.Application.Integration/Events/TestEvent.cs
@@ -1,3 +1,3 @@
 ﻿namespace Shared.Application.Integration.Events;
 
-public record TestEvent(string Value) : IEvent; // TODO Remove
+public record TestEvent(string Value, DateTimeOffset CreatedAt) : IEvent; // TODO Remove
