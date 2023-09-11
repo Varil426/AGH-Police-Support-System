01522e6694827de1332679fedf23f18637b040f1
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-09-11 22:36:42 +0200

    Test Handlers

---
+++ PoliceSupportSystem/GunService.Application/Handlers/TestHandler.cs
@@ -0,0 +1,14 @@
+﻿// using Shared.Application.Handlers;
+// using Shared.Application.Integration.Events;
+//
+// namespace GunService.Application.Handlers;
+//
+// public class TestHandler : IEventHandler<IncidentCreatedEvent>
+// {
+//     public Task Handle(IncidentCreatedEvent @event)
+//     {
+//         Console.WriteLine("TEST");
+//         return Task.CompletedTask;
+//     }
+// }
+// TODO Remove
---
+++ PoliceSupportSystem/HqService.Application/Handlers/TestIncidentCreatedHandler.cs
@@ -0,0 +1,14 @@
+﻿// using Shared.Application.Handlers;
+// using Shared.Application.Integration.Events;
+//
+// namespace HqService.Application.Handlers;
+//
+// internal class TestIncidentCreatedHandler : IEventHandler<IncidentCreatedEvent>
+// {
+//     public Task Handle(IncidentCreatedEvent @event)
+//     {
+//         Console.WriteLine("TEST FROM HQ");
+//         return Task.CompletedTask;
+//     }
+// }
+// TODO Remove
