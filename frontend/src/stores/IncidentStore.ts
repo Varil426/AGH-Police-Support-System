a69ea3b0f16dc0efad8e2e3f998a5d30a2b72274
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-10-08 22:26:34 +0200

    IncidentStore - added action decorator

--- frontend/src/stores/IncidentStore.ts
+++ frontend/src/stores/IncidentStore.ts
@@ -1,15 +1,17 @@
-import { observable } from "mobx";
+import { action, observable } from "mobx";
 import { Incident } from "../models/Incident";
 import { IIncidentDto } from "../api/generated/Shared/Application/Integration/DTOs/IIncidentDto";
 
 export class IncidentStore {
   @observable readonly incidents: Incident[] = [];
 
+  @action
   addIncident(dto: IIncidentDto) {
     if (this.incidents.find((x) => x.id === dto.id)) return;
     this.incidents.push(dto);
   }
 
+  @action
   removeIncident(id: string) {
     const f = this.incidents.find((x) => x.id === id);
     if (f) {
@@ -18,6 +20,7 @@ export class IncidentStore {
     }
   }
 
+  @action
   updateIncident(dto: IIncidentDto) {
     const u = this.incidents.find((x) => x.id === dto.id);
     if (!u) return;
@@ -26,6 +29,7 @@ export class IncidentStore {
     u.type = dto.type;
   }
 
+  @action
   updateOrCreateIncident(dto: IIncidentDto) {
     if (this.incidents.some((x) => x.id === dto.id))
       return this.updateIncident(dto);
