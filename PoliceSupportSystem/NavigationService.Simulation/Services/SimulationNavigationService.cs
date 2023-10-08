8ceaae8c229a09bd9bfc18043918cc5f729c9044
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-10-08 14:14:30 +0200

    Added NavigationService.Simulation

---
+++ PoliceSupportSystem/NavigationService.Simulation/Services/SimulationNavigationService.cs
@@ -0,0 +1,17 @@
+﻿using NavigationService.Application.Services;
+using Shared.CommonTypes.Geo;
+
+namespace NavigationService.Simulation.Services;
+
+public class SimulationNavigationService : INavigationService
+{
+    internal Position? LastKnowPosition { private get; set; }
+
+    public async Task<Position> GetCurrentPosition()
+    {
+        while (LastKnowPosition == null) 
+            await Task.Delay(TimeSpan.FromMilliseconds(50));
+
+        return LastKnowPosition;
+    }
+}
---
+++ PoliceSupportSystem/NavigationService.Simulation/SimulationModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using NavigationService.Application.Services;
+using NavigationService.Simulation.Services;
+
+namespace NavigationService.Simulation;
+
+public class SimulationModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+
+        builder.RegisterType<SimulationNavigationService>().AsSelf().As<INavigationService>().SingleInstance();
+    }
+}
