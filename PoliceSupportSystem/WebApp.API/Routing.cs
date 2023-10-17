a2ff77fa349905938f7e5084c4544277898b718e
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-10-17 22:11:45 +0200

    WebApp - Added health check

--- PoliceSupportSystem/WebApp.API/Routing.cs
+++ PoliceSupportSystem/WebApp.API/Routing.cs
@@ -1,4 +1,4 @@
-﻿using WebApp.API.Hubs;
+﻿using System.Net;
 using WebApp.API.Hubs.MonitoringHub;
 
 namespace WebApp.API;
@@ -7,10 +7,12 @@ public static class Routing
 {
     private const string ApiPrefix = "/api";
     private const string MonitoringRoute = $"{ApiPrefix}/monitoring";
+    private const string HealthCheckRoute = $"{ApiPrefix}/health-check";
     
     public static WebApplication AddRouting(this WebApplication app)
     {
         app.MapHub<MonitoringHub>(MonitoringRoute);
+        app.MapGet(HealthCheckRoute, () => HttpStatusCode.OK);
         return app;
     }
 }
