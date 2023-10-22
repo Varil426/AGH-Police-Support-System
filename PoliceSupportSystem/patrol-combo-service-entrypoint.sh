d7cf42e5cee2b527de0f089421a6f12efbf29c3e
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-10-22 17:52:29 +0200

    Docker - Patrol - Set correct IDs

--- PoliceSupportSystem/patrol-combo-service-entrypoint.sh
+++ PoliceSupportSystem/patrol-combo-service-entrypoint.sh
@@ -3,17 +3,20 @@
 set -m
 
 pushd /App/NavigationService
-dotnet ./NavigationService.Service.dll --environment=Docker &
+PoliceSupportSystem_ServiceSettings__Id="NavigationService-${PoliceSupportSystem_PatrolSettings__NavAgentId}" \
+    dotnet ./NavigationService.Service.dll --environment=Docker &
 popd 
 
 sleep 4
 
 pushd /App/PatrolService
-dotnet ./PatrolService.Service.dll --environment=Docker &
+PoliceSupportSystem_ServiceSettings__Id="PatrolService-${PoliceSupportSystem_PatrolSettings__PatrolAgentId}" \
+    dotnet ./PatrolService.Service.dll --environment=Docker &
 popd
 
 pushd /App/GunService
-# dotnet ./GunService.Service.dll --environment=Docker &
+# PoliceSupportSystem_ServiceSettings__Id="GunService-${PoliceSupportSystem_PatrolSettings__GunAgentId}" \
+#     dotnet ./GunService.Service.dll --environment=Docker &
 popd
 
 fg %1
