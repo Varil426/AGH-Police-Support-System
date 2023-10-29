dd63b502361afe6c233f1e0be32d68f73ecf79cd
Bartłomiej Kręgielewski <bartlomiejkregielewski@gmail.com>
2023-10-30 00:03:12 +0100

    GunService - Added to Docker

--- PoliceSupportSystem/patrol-combo-service-entrypoint.sh
+++ PoliceSupportSystem/patrol-combo-service-entrypoint.sh
@@ -7,16 +7,16 @@ PoliceSupportSystem_ServiceSettings__Id="NavigationService-${PoliceSupportSystem
     dotnet ./NavigationService.Service.dll --environment=Docker &
 popd 
 
+pushd /App/GunService
+PoliceSupportSystem_ServiceSettings__Id="GunService-${PoliceSupportSystem_PatrolSettings__GunAgentId}" \
+    dotnet ./GunService.Service.dll --environment=Docker &
+popd
+
 sleep 4
 
 pushd /App/PatrolService
 PoliceSupportSystem_ServiceSettings__Id="PatrolService-${PoliceSupportSystem_PatrolSettings__PatrolAgentId}" \
     dotnet ./PatrolService.Service.dll --environment=Docker &
-popd
-
-pushd /App/GunService
-# PoliceSupportSystem_ServiceSettings__Id="GunService-${PoliceSupportSystem_PatrolSettings__GunAgentId}" \
-#     dotnet ./GunService.Service.dll --environment=Docker &
 popd
 
 fg %1
